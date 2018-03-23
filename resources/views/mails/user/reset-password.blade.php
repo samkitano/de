fadd00c8409c86e2b524f8b83448d11fe1662ab3
Sam Kitano <sam.kitano@gmail.com>
2018-03-23 20:01:42 +0000

    refactored

--- resources/views/mails/user/reset-password.blade.php
+++ resources/views/mails/user/reset-password.blade.php
@@ -1,21 +1,21 @@
 @component('mail::message')
-# {{ __t('mail.reset_password') }}
+# {{ __('passwords.reset_password') }}
 
-{{ __t('mail.hi') }}, {{ $user->first_name }}!
+{{ __('passwords.hi') }}, {{ $user->first_name }}!
 
-{{ __t('mail.requested_pw') }} [{{ Request::getHost() }}]({{ action('Front\Auth\ResetPasswordController@showResetForm', [$token]) }}?email={{ urlencode($user->email) }}).
+{{ __('passwords.requested_pw') }} [{{ Request::getHost() }}]({{ 'recover/'.$token }}?email={{ urlencode($user->email) }}).
 
-@component('mail::button', ['url' => action('Front\Auth\ResetPasswordController@showResetForm', [$token]).'?email='.urlencode($user->email)])
-{{ __t('mail.change_password') }}
+@component('mail::button', ['url' => 'recover/'.$token.'?email='.urlencode($user->email)])
+{{ __('passwords.change_password') }}
 @endcomponent
 
 @component('mail::panel')
-{{ __t('kcms.mail.change_pw_mistake') }}
+{{ __('passwords.change_pw_mistake') }}
 @endcomponent
 
 @slot('subcopy')
-{{ __t('mail.set_password_before') }} {{ Carbon\Carbon::now()->addDays(3)->format('d/m/Y') }}. {{ __t('mail.otherwise_expires') }}.
-{{ __t('mail.disclaimer') }}
+{{ __('passwords.set_password_before') }} {{ Carbon\Carbon::now()->addDays(3)->format('d/m/Y') }}
+{{ __('passwords.disclaimer') }}
 @endslot
 
 @slot('footer')
