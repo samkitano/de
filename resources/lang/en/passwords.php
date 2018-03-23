2c6d93b547aaec5854524466cb7022483b8b626e
Sam Kitano <sam.kitano@gmail.com>
2018-03-23 20:01:28 +0000

    added new lang lines

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,22 +1,17 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
+    'access_to' => 'Access to',
+    'change_password' => 'Change Password',
+    'change_pw_mistake' =>'You can safely ignore this email if you did not make such request.',
+    'disclaimer' => 'If you are not the intended recipient for this email, you can just ignore it. If you keep receiving them, please contact us.',
+    'hi' => 'Hi',
     'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'requested_pw' => 'A password reset has been requested for this account at ',
     'reset' => 'Your password has been reset!',
+    'reset_password' => 'Reset Password',
     'sent' => 'We have e-mailed your password reset link!',
+    'set_password_before' => 'This link will expire ',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
-
 ];
