cbeccec00a36de04079f20fe5d9d92427c2d9cbf
Sam Kitano <sam.kitano@gmail.com>
2018-03-24 23:14:06 +0000

    changed field lengths

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('nick_name')->unique();
+            $table->string('first_name', 30);
+            $table->string('last_name', 30);
+            $table->string('nick_name', 40)->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->datetime('last_active')->nullable();
