571f996a4942d2068c2a4e126cbf6dbdc49c4477
Sam Kitano <sam.kitano@gmail.com>
2018-03-24 16:31:50 +0000

    first commit

---
+++ resources/assets/js/components/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="user-profile">
+    <div class="container mx-auto mt-16 p-4">
+      {{ user}}
+    </div>
+  </section>
+</template>
+
+<script>
+  const wuser = window.user
+
+  export default {
+    beforeMount () {
+      this.user = wuser
+    },
+
+    data () {
+      return {
+        user: {}
+      }
+    },
+
+    name: 'Profile'
+  }
+</script>
