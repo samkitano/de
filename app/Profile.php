714c552e16d3a064afad1d4e7cc2c24814130ff4
Sam Kitano <sam.kitano@gmail.com>
2018-03-24 14:35:18 +0000

    first commit

---
+++ app/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'xp', 'bio', 'use_nick', 'show_age', 'changed_nick',
+    ];
+
+    /**
+     * User Relationship
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
