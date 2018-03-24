762ddb9191d0c6691ebfc2dce501245db7ae23f3
Sam Kitano <sam.kitano@gmail.com>
2018-03-24 19:44:27 +0000

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-//
 Auth::routes();
-//
-//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::any('{all}', function () {
     return view('front.index');
