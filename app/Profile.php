b356727970138a186d747d2edc9bec5d84c6355f
Sam Kitano <sam.kitano@gmail.com>
2018-03-24 15:03:46 +0000

    doc blocks

--- app/Profile.php
+++ app/Profile.php
@@ -5,6 +5,30 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Profile
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $xp
+ * @property string|null $bio
+ * @property int $use_nick
+ * @property int $show_age
+ * @property int $changed_nick
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereBio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereChangedNick($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereShowAge($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUseNick($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereXp($value)
+ * @mixin \Eloquent
+ */
 class Profile extends Model
 {
     /**
