8e672787d4d6f27bc6b3977f39b9c0e5c8ffcaa4
Sam Kitano <sam.kitano@gmail.com>
2018-03-24 11:22:31 +0000

    json responses

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,30 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
-
     use ResetsPasswords;
-
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -35,5 +18,17 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /** @inheritdoc */
+    protected function sendResetResponse($response)
+    {
+        return response()->json(__user(), 200);
+    }
+
+    /** @inheritdoc */
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return response()->json(['errors' => ['email' => trans($response)]], 422);
     }
 }
