12636ed088320a8828c6193a3f3164f329f3ac46
Sam Kitano <sam.kitano@gmail.com>
2018-03-25 20:18:20 +0100

    added working actions

--- resources/assets/js/store/actions.js
+++ resources/assets/js/store/actions.js
@@ -1,3 +1,11 @@
 export const setUser = ({ commit }, user) => {
   commit('SET_USER', user)
 }
+
+export const setWorking = ({ commit }) => {
+  commit('SET_WORKING', true)
+}
+
+export const unsetWorking = ({ commit }) => {
+  commit('SET_WORKING', false)
+}
