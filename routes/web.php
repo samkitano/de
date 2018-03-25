6650ed7c40b08d2ef0b22f9bd8bc6053188f683e
Sam Kitano <sam.kitano@gmail.com>
2018-03-25 14:57:59 +0100

    api routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 Auth::routes();
 
+Route::group([
+    'namespace' => 'Api',
+    'middleware' => 'auth',
+    'prefix' => 'api'
+], function () {
+    Route::get('profile/{id}', 'MyProfileController@show');
+    Route::patch('profile/{id}', 'MyProfileController@update');
+});
+
 Route::any('{all}', function () {
     return view('front.index');
 })->where(['all' => '.*']);
