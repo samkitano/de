74fbde282dfad04c4d05ebb626df93ac39f94170
Sam Kitano <sam.kitano@gmail.com>
2018-03-25 20:19:22 +0100

    working state on vuex

--- resources/assets/js/components/partials/_editable-user-first-name.vue
+++ resources/assets/js/components/partials/_editable-user-first-name.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="editable-content mb-4">
-    <div class="mb-1 text-2xl">
+    <div class="mb-1 flex">
       <strong>Nome: </strong
       ><span v-on:blur="cancel"
              @keydown.13="change"
              @keydown.27="cancel"
              ref="first_name"
              @click="cleanup"
+             :disabled="$store.state.working"
+             class="ml-2"
              contenteditable="true">{{ $store.state.user.first_name }}</span
-    ></div>
+    ><span class="ml-2 text-red" v-show="$store.state.working" v-html="loader"></span></div>
 
     <p class="error">{{ first_name.feedback }}</p>
   </div>
@@ -42,22 +44,24 @@
     },
 
     methods: {
-      ...mapActions(['setUser']),
+      ...mapActions(['setUser', 'setWorking', 'unsetWorking']),
 
       cancel () {
         document.execCommand('undo')
       },
 
       change (e) {
+        this.setWorking()
         let name = this.$refs.first_name.innerText.trim()
 
-        e.target.blur()
+        this.$refs.first_name.blur()
         e.preventDefault()
 
         this.first_name.value = name
 
         if (this.invalidName) {
           this.first_name.feedback = 'O nome é obrigatório e só pode conter letras e pontuação.'
+          this.unsetWorking()
           return false
         }
 
@@ -66,9 +70,11 @@
         axios.post(`/api/profile/${this.$store.state.user.id}`, this.payload)
           .then((r) => {
             this.setUser(r.data.user)
+            this.unsetWorking()
             this.$swal('Feito!', 'Nome alterado!', 'success')
           })
           .catch((e) => {
+            this.unsetWorking()
             if (e.response.status === 422) {
               this.showValidationErrors(e.response.data.errors)
             } else {
